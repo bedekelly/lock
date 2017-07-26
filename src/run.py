d475dabfe7480f13258b13977986cbae98fb497f
Bede Kelly <bedekelly97@gmail.com>
2017-07-26 14:30:03 +0100

    Add default port; turn off debug mode.

--- src/run.py
+++ src/run.py
@@ -6,9 +6,11 @@ from lock_app import views
 from lock_app.keys import INITIAL_KEY
 from lock_app.password import save_key
 
+PORT = 8765
+
 save_key(INITIAL_KEY)
 app = Flask(__name__)
 CORS(app)
 views.init(app)
 
-app.run(debug=True)
+app.run(port=PORT)
